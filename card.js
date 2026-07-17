e4c4c118672fdf1908c5a0d0d37127851df4127a
rxuhi <hyorim4640@gmail.com>
2026-07-17 15:15:15 +0900

    Update card.js

--- card.js
+++ card.js
@@ -11,7 +11,7 @@ const SUB_COLOR = '#2b2d31';
 let FONT_FAMILY = 'sans-serif';
 let FONT_FAMILY_BOLD = 'sans-serif';
 try {
-  const fontPath = path.join(__dirname, 'fonts', '유한킴벌리_푸른숲체 Bold.ttf');
+  const fontPath = path.join(__dirname, '유한킴벌리_푸른숲체 Bold.ttf');
 
 if (fs.existsSync(fontPath)) {
     GlobalFonts.registerFromPath(fontPath, 'Yuhan');
